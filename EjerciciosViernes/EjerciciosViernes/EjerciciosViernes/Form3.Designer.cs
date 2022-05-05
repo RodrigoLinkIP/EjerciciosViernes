602825c3fd466f9604878ac37b0c5a7eb5be9b86
RodrigoLinkIP <rodrigolopezlopez2006@gmail.com>
2022-05-05 14:42:41 -0600

    Revisado Nuevamente

--- EjerciciosViernes/EjerciciosViernes/EjerciciosViernes/Form3.Designer.cs
+++ EjerciciosViernes/EjerciciosViernes/EjerciciosViernes/Form3.Designer.cs
@@ -119,7 +119,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(13, 131);
+            this.button4.Location = new System.Drawing.Point(14, 131);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 8;
